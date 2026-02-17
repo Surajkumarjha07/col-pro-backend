f6c2dd0b80b97c75741106885247ed5f8ae541d6
Suraj Kumar Jha <surajkumarjha@mogiio.com>
2026-02-17 16:54:29 +0530

    deployed successfully!

--- src/index.js
+++ src/index.js
@@ -22,12 +22,6 @@ if (process.env.NODE_ENV !== "production") {
     });
 }
 
-app.use((req, res, next) => {
-  console.log("Incoming Origin:", req.headers.origin);
-  next();
-});
-
-
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors({
