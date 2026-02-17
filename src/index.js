68fa0aa7fa45035b3e18612a274b0d8d71b5bac6
Suraj Kumar Jha <surajkumarjha@mogiio.com>
2026-02-17 16:47:23 +0530

    CORS issue resolved

--- src/index.js
+++ src/index.js
@@ -22,6 +22,12 @@ if (process.env.NODE_ENV !== "production") {
     });
 }
 
+app.use((req, res, next) => {
+  console.log("Incoming Origin:", req.headers.origin);
+  next();
+});
+
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors({
